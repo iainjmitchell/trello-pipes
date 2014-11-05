ffc5a956e76efc313cb9ddc6a0f9d7c10e17f099
iainjmitchell <iainjmitchell@gmail.com>
2014-11-05 17:27:53 +0000

    no cards produced when lists but no cards

--- tests/CardsAfterAndIncludingListProducerTests.rb
+++ tests/CardsAfterAndIncludingListProducerTests.rb
@@ -8,7 +8,7 @@ module TrelloPipes
 			@successor = successor
 		end
 
-		def produce
+		def produce(end_list_name)
 			@successor.push([])
 		end
 	end
@@ -18,13 +18,23 @@ class CardsAfterAndIncludingListProducerTests < Test::Unit::TestCase
 	include TrelloPipes
 
 	def test_that_nothing_is_produced_when_board_has_no_lists
+		list_name = "a list #{SecureRandom.uuid}"
 		mock_successor = MockSuccessor.new
 		board_with_no_lists = FakeBoard.new
 		producer = CardsAfterAndIncludingListProducer.new(mock_successor, board_with_no_lists)
-		producer.produce
+		producer.produce(list_name)
 		expect(mock_successor.pushed_cards.length).to eql(0)
 	end
 
+	def test_that_nothing_is_produced_when_board_has_list_BUT_no_cards
+		list_name = "a list #{SecureRandom.uuid}"
+		mock_successor = MockSuccessor.new
+		board_with_list = FakeBoard.new
+		board_with_list.add(FakeList.new(list_name))
+		producer = CardsAfterAndIncludingListProducer.new(mock_successor, board_with_list)
+		producer.produce(list_name)
+		expect(mock_successor.pushed_cards.length).to eql(0)
+	end
 
 end
 
@@ -45,5 +55,18 @@ class FakeBoard
 
 	def add(list)
 		@lists.push(list)
+	end
+end
+
+class FakeList
+	attr_reader :name, :cards
+
+	def initialize(name)
+		@name = name
+		@cards = []
+	end
+
+	def add(card)
+		@cards.push(card)
 	end
 end
