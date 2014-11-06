321bcff986616d8fdd532d8258cff856ff7603cd
iainjmitchell <iainjmitchell@gmail.com>
2014-11-06 12:00:38 +0000

    starting card has complexity filter

---
+++ tests/filters/CardContainsComplexityFilterTests.rb
@@ -0,0 +1,34 @@
+require 'test/unit'
+require 'rspec-expectations'
+require 'securerandom'
+
+class CardContainsComplexityFilter
+	def initialize(successor)
+		@successor = successor
+	end
+
+	def push(cards)
+		@successor.push(cards)
+	end
+end
+
+
+class CardContainsComplexityFilterTests < Test::Unit::TestCase
+	def test_no_cards_in_no_cards_out
+		mock_successor = MockSuccessor.new
+		no_cards = []
+		CardContainsComplexityFilter.new(mock_successor).push(no_cards)
+		expect(mock_successor.pushed_cards).to eql([])
+	end
+end
+
+class FakeCard
+end
+
+class MockSuccessor 
+	attr_reader :pushed_cards 
+
+	def push(cards)
+		@pushed_cards = cards
+	end
+end
