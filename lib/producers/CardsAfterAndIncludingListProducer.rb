4c3a447dd7a959b399217728fee52d94b4199090
iainjmitchell <iainjmitchell@gmail.com>
2014-11-06 11:38:10 +0000

    refactoring

---
+++ lib/producers/CardsAfterAndIncludingListProducer.rb
@@ -0,0 +1,27 @@
+module TrelloPipes
+	class CardsAfterAndIncludingListProducer
+		def initialize(successor, trello_board)
+			@successor = successor
+			@trello_board = trello_board
+		end
+
+		def produce(list_name)
+			lists = get_lists_after_and_including(list_name)
+			cards = all_cards_from(lists)
+			@successor.push(cards)
+		end
+
+		private
+		def get_lists_after_and_including(list_name)
+			list_index = @trello_board.lists.index { | list | list.name.match(list_name) }
+			return [] if list_index.nil?
+			@trello_board.lists.slice(list_index, @trello_board.lists.size)
+		end
+
+		def all_cards_from(lists)
+			return [] if lists.empty?
+			head, *tail = lists
+			return head.cards + all_cards_from(tail)
+		end
+	end
+end
--- tests/CardsAfterAndIncludingListProducerTests.rb
+++ tests/CardsAfterAndIncludingListProducerTests.rb
@@ -1,34 +1,7 @@
 require 'test/unit'
 require 'rspec-expectations'
 require 'securerandom'
-
-module TrelloPipes
-	class CardsAfterAndIncludingListProducer
-		def initialize(successor, trello_board)
-			@successor = successor
-			@trello_board = trello_board
-		end
-
-		def produce(list_name)
-			lists = get_lists_after_and_including(list_name)
-			cards = all_cards_from(lists)
-			@successor.push(cards)
-		end
-
-		private
-		def get_lists_after_and_including(list_name)
-			list_index = @trello_board.lists.index { | list | list.name.match(list_name) }
-			return [] if list_index.nil?
-			@trello_board.lists.slice(list_index, @trello_board.lists.size)
-		end
-
-		def all_cards_from(lists)
-			return [] if lists.empty?
-			head, *tail = lists
-			return head.cards + all_cards_from(tail)
-		end
-	end
-end
+require_relative '../lib/producers/CardsAfterAndIncludingListProducer'
 
 class CardsAfterAndIncludingListProducerTests < Test::Unit::TestCase
 	include TrelloPipes
