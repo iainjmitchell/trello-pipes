4ef725838a263d7bc7063aa0464441c43ca8509e
iainjmitchell <iainjmitchell@gmail.com>
2014-11-06 12:04:20 +0000

    one card with complexity

--- tests/filters/CardContainsComplexityFilterTests.rb
+++ tests/filters/CardContainsComplexityFilterTests.rb
@@ -24,9 +24,22 @@ class CardContainsComplexityFilterTests < Test::Unit::TestCase
 		CardContainsComplexityFilter.new(mock_successor).push(no_cards)
 		expect(mock_successor.pushed_cards).to eql([])
 	end
+
+	def test_one_card_with_complexity
+		mock_successor = MockSuccessor.new
+		card_with_complexity = FakeCard.new('(1) Hello')
+		one_card_with_complexity = [card_with_complexity]
+		CardContainsComplexityFilter.new(mock_successor).push(one_card_with_complexity)
+		expect(mock_successor.pushed_cards).to eql(one_card_with_complexity)
+	end
 end
 
 class FakeCard
+	attr_reader :name
+
+	def initialize(name = 'no name')
+		@name = name
+	end
 end
 
 class MockSuccessor 
