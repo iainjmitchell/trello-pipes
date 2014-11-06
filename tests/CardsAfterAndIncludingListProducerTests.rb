ce4f6918df6b9d42805584bd07ef7c7de49cfb33
iainjmitchell <iainjmitchell@gmail.com>
2014-11-06 08:40:27 +0000

    multiple cards in list

--- tests/CardsAfterAndIncludingListProducerTests.rb
+++ tests/CardsAfterAndIncludingListProducerTests.rb
@@ -6,10 +6,13 @@ module TrelloPipes
 	class CardsAfterAndIncludingListProducer
 		def initialize(successor, trello_board)
 			@successor = successor
+			@trello_board = trello_board
 		end
 
 		def produce(end_list_name)
-			@successor.push([])
+			cards = []
+			cards = @trello_board.lists[0].cards if @trello_board.lists.length > 0
+			@successor.push(cards)
 		end
 	end
 end
@@ -36,6 +39,26 @@ class CardsAfterAndIncludingListProducerTests < Test::Unit::TestCase
 		expect(mock_successor.pushed_cards.length).to eql(0)
 	end
 
+	def test_that_multiple_cards_are_produced_when_board_has_list_with_multiple_cards
+		list_name = "a list #{SecureRandom.uuid}"
+		card1 = FakeCard.new
+		card2 = FakeCard.new
+		card3 = FakeCard.new
+		mock_successor = MockSuccessor.new
+		board_with_list = FakeBoard.new
+		list_with_multiple_cards = FakeList.new(list_name)
+		list_with_multiple_cards
+			.add(card1)
+			.add(card2)
+			.add(card3)
+		board_with_list.add(list_with_multiple_cards)
+		producer = CardsAfterAndIncludingListProducer.new(mock_successor, board_with_list)
+		producer.produce(list_name)
+		expect(mock_successor.pushed_cards).to eql([card1, card2, card3])
+	end
+end
+
+class FakeCard
 end
 
 class MockSuccessor 
@@ -68,5 +91,6 @@ class FakeList
 
 	def add(card)
 		@cards.push(card)
+		self
 	end
 end
