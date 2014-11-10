f3963fa2297da6d3524f81970d61a87c8dc3f2a5
iainjmitchell <iainjmitchell@gmail.com>
2014-11-10 11:16:06 +0000

    completed velocity calculator

--- tests/adapters/VelocityCalculatorAdapterTests.rb
+++ tests/adapters/VelocityCalculatorAdapterTests.rb
@@ -8,7 +8,22 @@ module TrelloPipes
 		end
 
 		def push(cards)
-			@successor.push(0)
+			complexity = 0
+			complexity = cards.inject(complexity) do |complexity, card|
+				complexity + CardComplexity.new(card).complexity
+			end
+			@successor.push(complexity)
+		end
+	end
+
+	class CardComplexity 
+		attr_reader :complexity
+
+		COMPLEXITY_REGEX = /\(\d*\)/
+
+		def initialize(trello_card)
+			complexity_match = trello_card.name.match(COMPLEXITY_REGEX)
+			@complexity = (complexity_match.to_s.delete! '()').to_i
 		end
 	end
 end
@@ -22,6 +37,39 @@ class VelocityCalculatorAdapterTests < Test::Unit::TestCase
 		VelocityCalculatorAdapter.new(mock_display).push(no_cards)
 		expect(mock_display.velocity).to eql(0)
 	end
+
+	def test_one_card_with_velocity
+		velocity = rand(1..13)
+		mock_display = MockDisplay.new
+		one_card = [FakeCard.new("(#{velocity}) bob")]
+		VelocityCalculatorAdapter.new(mock_display).push(one_card)
+		expect(mock_display.velocity).to eql(velocity)
+	end
+
+	def test_multiple_cards_with_velocity
+		velocity1 = rand(1..13)
+		velocity2 = rand(1..13)
+		velocity3 = rand(1..13)
+		mock_display = MockDisplay.new
+		multiple_cards = [
+			FakeCard.new("(#{velocity1}) bob1"),
+			FakeCard.new("(#{velocity2}) bob2"),
+			FakeCard.new("(#{velocity3}) bob3")
+		]
+		VelocityCalculatorAdapter.new(mock_display).push(multiple_cards)
+		expect(mock_display.velocity).to eql(velocity1 + velocity2 + velocity3)
+	end
+
+	def test_one_card_with_velocity_and_one_without
+		velocity = rand(1..13)
+		mock_display = MockDisplay.new
+		one_card_with_velocity_and_one_without = [
+			FakeCard.new("bob1 (#{velocity})"),
+			FakeCard.new("bob2")
+		]
+		VelocityCalculatorAdapter.new(mock_display).push(one_card_with_velocity_and_one_without)
+		expect(mock_display.velocity).to eql(velocity)
+	end
 end
 
 class MockDisplay
@@ -29,5 +77,13 @@ class MockDisplay
 
 	def push(velocity)
 		@velocity = velocity
+	end
+end
+
+class FakeCard
+	attr_reader :name
+
+	def initialize(name)
+		@name = name
 	end
 end
